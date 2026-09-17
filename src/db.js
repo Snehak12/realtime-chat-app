b1a07f1010d524090abbe7b9c4b585c56860fd12
Snehak Tarun Tiu <snehaktiu04@gmail.com>
2026-09-17 17:29:31 +0530

    Prepare backend for deployment

--- src/db.js
+++ src/db.js
@@ -1,13 +1,20 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    max: 10
-});
+const poolConfig = process.env.DATABASE_URL
+    ? {
+          connectionString: process.env.DATABASE_URL,
+          max: 10
+      }
+    : {
+          user: process.env.DB_USER,
+          host: process.env.DB_HOST,
+          database: process.env.DB_NAME,
+          password: process.env.DB_PASSWORD,
+          port: process.env.DB_PORT,
+          max: 10
+      };
+
+const pool = new Pool(poolConfig);
 
 module.exports = pool;
--- src/server.js
+++ src/server.js
@@ -31,7 +31,7 @@ app.use(userRoutes);
 
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
